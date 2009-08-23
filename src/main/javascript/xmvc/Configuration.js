379ef77dabdf78f93646145e9c6b91b9f1723b3c
scott <scott@ebfa9934-6f10-0410-9599-894cdf3d53f0>
2009-08-23 23:21:06 +0000

    Added configuration parser (untested)
     * Builds objects defined that are temporarily defined in `Transformer.js`
       through composition based on XML input defined in `src/xml/controller.xsd`.
     * Depends on new methods from controller:
       * getTransformer(type, source) [ to be renamed to createTransformer ]
       * getMutator(mutationType) returns the appropriately registered mutator
         object.
       * createAction(objectName, methodName, observer) factory for the Action
         object.
       * createLocator(type, path) factory for the various Locator objects.
       * createSelection(locator) factory for the Selection object.
    
    TODO:
     * Think about renaming locator to "query" or something similar.
     * Added the factory methods above.
     * Create a POC.
     * Get the POC working!
    
    git-svn-id: http://svn.blisted.org/xmvc/trunk@142 ebfa9934-6f10-0410-9599-894cdf3d53f0

---
+++ src/main/javascript/xmvc/Configuration.js
@@ -0,0 +1,158 @@
+
+function XMVCConfigParser () {
+    var nodeRules = [
+        { included: [ "id" ],
+          excluded: [ "context", "type", "path" ] },
+        { included: [ "type", "path" ],
+          excluded: [ "id", "context" ] },
+        { included: [ "context" ],
+          excluded: [ "id", "type", "path" ] },
+    ]
+
+    this.validateAttributes = function (node, rules) {
+        var valid = true
+
+        rules.each(function (rule) {
+            rule.included.each(function (included) {
+                if (valid) valid = node.hasAttribute(included)
+            })
+
+            rule.excluded.each(function (excluded) {
+                if (valid) valid = !node.hasAttribute(excluded)
+            })
+        })
+
+        return valid
+    }
+
+    this._buildTransformer = function (transform) {
+        var source   = transform.getAttribute("src")
+        var type     = transform.getAttribute("type")
+
+        var transformer = controller.createTransformer(type, source)
+    }
+
+    this._nextTransformNode = function (transform) {
+        for (var node = transform.firstChild;
+                 node != null;
+                 node = node.nextSibling) {
+            if (node.localName == "transform") {
+                return node
+            }
+        }
+
+        return null
+    }
+
+    this._buildTransformObserver = function (transform) {
+        var mutation = "replace"
+
+        if (transform.hasAttribute("behavior")) {
+            mutation = transform.getAttribute("behavior")
+        }
+
+        var transformers = []
+
+        for (var node = transform;
+                 node != null;
+                 node = this._nextTransformNode(node)) 
+        {
+            var transformer = this._buildTransformer(transform)
+
+            /* Prepend to the list, they need to be processed from lowest
+             * descendant to parent */
+            transformers.unshift(transformer)
+        }
+
+        var locator     = this._buildLocator(transform)
+        var mutator     = controller.getMutator(mutation)
+
+        var transformObserver = new TransformObserver(
+            transformers, locator, mutator)
+
+        return transformObserver
+    }
+
+    this._buildAction = function (observer) {
+        return controller.createAction(
+            action.getAttribute("object"),
+            action.getAttribute("method"),
+            observer)
+    }
+
+    this._buildLocator = function (node) {
+        var locateType = null
+        var locatePath = null 
+
+        if (node.hasAttribute("id")) {
+            locateType = "id"
+            locatePath = node.getAttribute("id")
+        }
+        if (node.hasAttribute("context")) {
+            locateType = "xpath"
+            locatePath = node.getAttribute("context")
+        }
+        if (node.hasAttribute("type")) {
+            locateType = node.getAttribute("locator")
+            locatePath = node.getAttribute("path")
+        }
+
+        return controller.createLocator(locateType, locatePath)
+    }
+
+    this._buildSelection = function (node) {
+        var locator   = this._buildLocator(node)
+        var selection = controller.createSelection(locator)
+
+        for (var child = node.firstChild;
+                 child != null;
+                 child = child.nextSibling)
+        {
+            if (child.localName == "action") {
+                /* TODO: Support multiple transform observers through the
+                 * listable transform interface. */
+                var transform = this._nextTransformNode(child)
+                var observer  = null
+
+                if (transform != null) {
+                    observer = this._buildTransformObserver(transform)
+                }
+                else {
+                    observer = new NoopObserver()
+                }
+
+                selection.addAction(
+                        child.getAttribute("type"),
+                        this._buildAction(child, observer)
+                        )
+            }
+        }
+
+        return selection
+    }
+
+    this._parseNodes = function (element) {
+        var selections = []
+
+        for (var node = element.firstChild; 
+             node != null; 
+             node = node.nextSibling)
+        {
+            if (node.type == Node.ELEMENT_NODE) {
+                if (node.localName == "node") {
+                    if (this.validateAttributes(node, nodeRules)) {
+                        selections.push(this._buildSelection(node))
+                    }
+                }
+            }
+        }
+    }
+
+    this.parse = function (element) {
+        if (element.type == Node.DOCUMENT_NODE) {
+            element = element.documentElement
+        }
+
+        var selections = this._parseNodes(element)
+    }
+}
